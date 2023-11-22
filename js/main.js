f9d19951399a183eca2e463e68e7d218bb9ada8e
bloxcosite <139221902+bloxcosite@users.noreply.github.com>
2023-11-22 22:14:02 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@
 /* Created (DD-MM-YYYY @ HH:MM): 08-07-2023 @ 18:48CEST */
 /* Use: This is the main file JavaScript, which is loaded onto all pages in the site. It contains all the necessary stuff. */
 
-const siteVersion = "NOV14-2023_1"
+const siteVersion = "NOV22-2023_1"
 
 const bloxcoMsgb64 = "IF8gICAgICBfICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAKfCB8ICAgIHwgfCAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAKfCB8X18gIHwgfCAgX19fICBfXyAgX18gIF9fXyAgIF9fXyAgICAgICBfX18gICBfIF9fICAgX18gXyAKfCAnXyBcIHwgfCAvIF8gXCBcIFwvIC8gLyBfX3wgLyBfIFwgICAgIC8gXyBcIHwgJ19ffCAvIF9gIHwKfCB8XykgfHwgfHwgKF8pIHwgPiAgPCB8IChfXyB8IChfKSB8IF8gfCAoXykgfHwgfCAgIHwgKF98IHwKfF8uX18vIHxffCBcX19fLyAvXy9cX1wgXF9fX3wgXF9fXy8gKF8pIFxfX18vIHxffCAgICBcX18sIHwKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgX18vIHwKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICB8X19fLyAKICAgICAgICAgICAgICAgICAgICBibG94Y28ub3JnICAgICAgICAgICAgICAKICAgICAgICAgICAgICAgICAgICBERVZFTE9QRVIgQ09OU09MRSAgICAgICAgICAgICAgICAgIAogICAgICAgICAgICAgICAgICAgIGJ5IHplcnR5YmVlXzUgICAgICAgICAgICAgICAgICAgICAgIA=="
 console.log(atob(bloxcoMsgb64) + "\n-------------\nWelcome to bloxco.org! Looking for bugs? Thanks! Please report all found bugs to zertybee on Discord.")
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const alertcontainer = document.getElementById('alert-container');
   
   const alerts = `
-  <div class="alert alert-blurple" style="display: block !important;">
+ <div class="alert alert-blurple" style="display: block !important;">
   <strong>
     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><style>svg{fill:#ffffff}</style><path d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"/></svg>
     <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
@@ -34,9 +34,39 @@ document.addEventListener('DOMContentLoaded', function() {
     <a href="http://www.google.com/url?q=http%3A%2F%2Fdiscord.gg%2FbPQMeZA4PW&sa=D&sntz=1&usg=AOvVaw37-CqJKaqRmebJ85BslNR5">Discord</a>, 
   and make new friends!
 </div>
+
+<div class="alert alert-warning" style="display: block !important;">
+<strong>
+<img src="https://cdn-icons-png.flaticon.com/512/5084/5084806.png" style="max-height: 25px;"/>
+  REFERRAL CONTEST</strong>:
+Refer friends, and win 🤑ROBUX*🤑 <a href="/referrals.html#:~:text=The%20creator%20of%20the%20code%20with%20the%20most%20uses%20wins%20🤑800%20ROBUX🤑!" class="text-warning fw-bold">More Information</a>
+</div>
+
+<!-- HROTM CLOSED <div class="alert alert-blurple" style="display: block !important;">
+<strong>
+  <img src="https://cdn.discordapp.com/emojis/1152674422952566875.webp?size=96&quality=lossless" style="max-height: 25px;"/>
+  HROTM</strong>:
+Votes for High Rank of The Month of October 2023 are out! 
+  <a href="http://www.google.com/url?q=http%3A%2F%2Fdiscord.gg%2FbPQMeZA4PW&sa=D&sntz=1&usg=AOvVaw37-CqJKaqRmebJ85BslNR5">Join our Discord server</a>, 
+and vote using /hrotm! You have <strong><span id="hrotmcountdown">loading time...</span></strong> remaining.
+</div> -->
+  
   `;
 
   alertcontainer.innerHTML = alerts;
+
+
+if (urlParams.get('loggedout') == "true") {
+  const loggedoutalert = `<div class="alert alert-success" style="display: block !important;">
+  <strong>
+    <img src="https://icons.veryicon.com/png/o/miscellaneous/batch-editor/success-38.png" height="25" style="filter: brightness(0)" draggable="false"></img>
+    Success</strong>:
+  You've been succesfully logged out.
+</div>
+</section>`
+alertcontainer.innerHTML = alerts + loggedoutalert
+}
+
 });
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -145,7 +175,6 @@ if (maintenance) {
                 or
                 <a href="https://discord.gg/bloxcosupermarket" class="btn btn-secondary text-white" style="color: white !important;">Join our Discord Server</a>
               </p>
-      </div>
   `
 
   if (document.getElementById('s2')) {
