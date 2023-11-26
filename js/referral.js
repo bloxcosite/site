2a3ce467854ee1f6f0cc77f3562245b1bfe94324
bloxcosite <139221902+bloxcosite@users.noreply.github.com>
2023-11-26 18:22:14 +0100

    Delete js directory

--- js/referral.js
+++
@@ -1,9 +0,0 @@
-/* https://www.roblox.com/games/start?placeId=9901611189&launchData= */
-const referralCode = urlParams.get('ref')
-
-if (referralCode) {
-    const joinurl = `https://www.roblox.com/games/start?placeId=9901611189&launchData=${referralCode}`
-    window.location.replace(
-        joinurl,
-      );
-}
