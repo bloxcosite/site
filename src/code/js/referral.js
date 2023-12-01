339192315bc1235d0b4d854bf9de0c2157c940b6
bloxcosite <139221902+bloxcosite@users.noreply.github.com>
2023-12-01 23:06:22 +0100

    [+-]: Referral Codes
    Disabled referral codes, added alert when someone tries to use a code

--- src/code/js/referral.js
+++ src/code/js/referral.js
@@ -2,8 +2,17 @@
 const referralCode = urlParams.get('ref')
 
 if (referralCode) {
-    const joinurl = `https://www.roblox.com/games/start?placeId=9901611189&launchData=${referralCode}`
+  /*  const joinurl = `https://www.roblox.com/games/start?placeId=9901611189&launchData=${referralCode}`
     window.location.replace(
         joinurl,
-      );
+      ); */
+    const inactive_alert = `
+    <div class="alert alert-danger" style="display: block !important;">
+    <strong>
+      <i class="fa-solid fa-xmark" style="color: #d00b0b;"></i>
+      Referrals</strong>:
+   Uh-oh! It seems like referral codes have been disabled. We're sorry!
+  </div>
+    `
+    document.getElementById('alert-container').innerHTML = document.getElementById('alert-container').innerHTML + inactive_alert
 }
